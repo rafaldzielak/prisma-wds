6c80af3f41b9dda411a4834167dfd9af4d513c42
rafaldzielak <69675572+rafaldzielak@users.noreply.github.com>
2023-04-10 18:24:26 +0200

    Simple usage

---
+++ script.ts
@@ -0,0 +1,12 @@
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+const main = async () => {
+  // const user = await prisma.user.create({ data: { name: "Dyrek" } });
+  const user = await prisma.user.findMany();
+  console.log(user);
+};
+
+main()
+  .catch((e) => console.error(e.message))
+  .finally(async () => await prisma.$disconnect());
